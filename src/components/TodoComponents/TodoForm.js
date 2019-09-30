099ce29667e45de09597fc7df8e26166e14cce10
Temitope-Akinsoto <takinsoto@gmail.com>
2019-09-30 10:17:39 -0700

    feat: Add event listener methods to TodoForm
    In this commit, i added two listener methods (handleInputChange and handlClick) to the TodoForm components.
    This event handlers are responsible for handling the onChange event when the user enters a new todo AND the onClick event whenever the todo is submitted

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,18 +1,36 @@
 import React from "react";
 
 export default class TodoForm extends React.Component {
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            userTodoInput: ''
+        }
+    }
+
+    handleInputChange = event => {
+        console.log('event is', event.target.value);
+        this.setState({
+            userTodoInput: event.target.value
+        })
+    }
+
+    handleClick = event => {
+        event.preventDefault();
+        console.log("I got triggered");
+    }
 
 
     render(){
         return(
             <form>
                 <div>
-                    <input type="text" />
-                    <button>Add Todo</button>
+                    <input value={this.state.userName} onChange={this.handleInputChange} />
+                    <button type="submit" onClick={this.handleClick}>Add Todo</button>
                     <button>Clear Completed</button>
                 </div>
-            </form>
-            
+            </form>            
         )
     }
 }
